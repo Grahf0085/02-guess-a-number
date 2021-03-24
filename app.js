fd56b6c5d613e978ffe72e5860f4d99a790619e6
Tucker Hoog <tuckerhoog@tutanota.com>
2021-03-24 18:38:07 -0400

    completed event listener to update state based on values

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
 // import functions and grab DOM elements
 
+import { compareNumbers } from './utils.js';
+
 // initialize state
 
 // set event listeners to update state and DOM
 
-const usersGuess = document.getElementById('usersGuess');
-const submitButton = document.getElementById('submitButton');
+const usersGuess = document.getElementById('users-guess');
+const submitButton = document.getElementById('submit-button');
 const hint = document.getElementById('hint');
-let remainingTrys = document.getElementById('remainingTrys');
-let realNumber = Math.ceil(Math.random() * 10);
+const remainingTrysText = document.getElementById('remaining-trys');
+let realNumber = Math.ceil(Math.random() * 20);
+let remainingTrysNumber = 3;
+
 
 submitButton.addEventListener('click', () => {
+    let usersNumber = usersGuess.valueAsNumber;
+    let isRight = compareNumbers(usersNumber, realNumber);
+
+    if (isRight === 1) {
+        hint.textContent = 'Too High! :P';
+        remainingTrysNumber--;
+        remainingTrysText.textContent = `${remainingTrysNumber}`;
+    }
+    if (isRight === -1) {
+        hint.textContent = 'Too Low! :P';
+        remainingTrysNumber--;
+        remainingTrysText.textContent = `${remainingTrysNumber}`;
+    }
+    if (isRight === 0) {
+        hint.textContent = 'You win! :D';
+        submitButton.style.display = 'none';
+    }
+
+    if (remainingTrysNumber === 0) {
+        hint.textContent = 'You Lost :(';
+
+    }
 
 });
 
